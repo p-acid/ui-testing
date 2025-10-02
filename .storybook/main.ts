7b99cb8b6b089835eb26a9336034d78431b97cf5
p-acid <qkrtkstjd01@gmail.com>
2025-10-02 20:35:32 +0900

    fix: build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,6 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
     "@storybook/addon-themes"
   ],
   "framework": {

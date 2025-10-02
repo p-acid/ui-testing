8896279d77c0a273e804f4c584a4133270a678fb
p-acid <qkrtkstjd01@gmail.com>
2025-10-02 16:30:00 +0900

    chore: migrate to ui package

---
+++ vite.config.lib.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'node:path';
+import dts from 'vite-plugin-dts';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      include: ['src/**/*'],
+      exclude: ['src/**/*.stories.*', 'src/**/*.test.*'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'UITesting',
+      formats: ['es', 'umd'],
+      fileName: (format) => `ui-testing.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
